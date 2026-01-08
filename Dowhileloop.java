80cc8bb9a11deeb2efe22c20d29bad21e2cf5cce
Ahmed Aden Noor Jama <123471853+6430Ahmed@users.noreply.github.com>
2026-01-08 14:32:08 +0300

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Dowhileloop{
+    public static void main(String[] args){
+        int i=1;
+        do {
+           System.out.println("Number:" +i);
+           i++;
+        }
+      while (i<=5);
+    }
+}
