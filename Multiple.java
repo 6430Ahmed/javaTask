0a4d76d2b67e89be97b8ed3b498c46e30dc9e052
Ahmed Aden Noor Jama <123471853+6430Ahmed@users.noreply.github.com>
2026-01-08 14:35:03 +0300

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+interface Animal {
+    void eat();
+}
+
+interface Pet {
+    void play();
+}
+
+class Dog implements Animal, Pet {
+
+    public void eat() {
+        System.out.println("Dog is eating.");
+    }
+
+    public void play() {
+        System.out.println("Dog is playing.");
+    }
+}
+
+public class Multiple {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.play();
+    }
+}
