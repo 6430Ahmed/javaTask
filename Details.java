11119edb3b545062f48fdd763c866affeb50dd2f
Ahmed Aden Noor Jama <123471853+6430Ahmed@users.noreply.github.com>
2026-01-08 14:21:08 +0300

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+public class Details {
+    public static void main(String[] args){
+        String name, city, address;
+        int age,telephone;
+        Scanner x=new Scanner(System.in);
+        System.out.print("Enter name:");
+        name = x.nextLine();
+        System.out.print("Enter address:");
+        address =x.nextLine();
+        System.out.print("Enter city:");
+        city= x.nextLine();
+        System.out.print("Enter telephone:");
+        telephone = x.nextInt();
+        System.out.print("Enter age:");
+        age= x.nextInt();
+        System.out.println("Welcome");
+    }
+}
